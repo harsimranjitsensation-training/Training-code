85ad3b79447c0a5ee0f776f8e7739f0b9497c033
Sagar kumar <srk001929@gmail.com>
2026-06-10 10:59:28 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,36 @@
+// console.log("hi there");
+
+// alert("welcome");
+
+
+// let name = "Abc";
+// name = "cd";
+// console.log(name);
+
+const pi = 3.14;
+
+var age = 18;
+
+
+let city = "Mohali"; //string
+let fruit = 'Mango'; //string
+
+console.log(typeof city);
+
+let marks = 65;  //number
+console.log(typeof marks); 
+
+let isLoggedin = true; //boolean
+console.log(typeof isLoggedin);
+
+let country; //undefined
+console.log(typeof country);  
+
+let school = null;  //null
+console.log(school); 
+
+let name = "your_name";
+
+console.log("my name is " + name);
+
+
